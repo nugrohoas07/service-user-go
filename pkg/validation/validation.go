566bff3e87d211459ea300a6e334aa8e0504f35d
nugroho alif <nugroho.alif77@gmail.com>
2024-05-08 11:09:33 +0700

    [IMPROVE] changing error message for advanced pass validation

--- pkg/validation/validation.go
+++ pkg/validation/validation.go
@@ -76,7 +76,7 @@ func formatMessage(err validator.FieldError) string {
 	case "containsany":
 		message = "does not contain required value"
 	case "advancedpass":
-		message = "password invalid"
+		message = "password must contain at least 1 uppercase letter, 1 lowercase letter, 1 number and 1 special character"
 	}
 
 	return message
